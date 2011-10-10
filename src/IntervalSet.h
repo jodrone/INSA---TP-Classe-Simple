6ea9520c1386097abe2a268ae163b239d67e0e58
Tristan Pourcelot <tristan.pourcelot@gmail.com>
2011-10-10 16:40:51 +0200

    Mise à jour des specifs sur l'interface

--- src/IntervalSet.h
+++ src/IntervalSet.h
@@ -5,7 +5,7 @@
     copyright            : (C) 2011 par Tristan Pourcelot & Jordan Vincent 
 *************************************************************************/
 
-//---------- Interface de la classe <IntervalSet> (fichier IntervalSet.h) ------
+//---- Interface de la classe <IntervalSet> (fichier IntervalSet.h) ------
 #if ! defined ( INTERVALSET_H )
 #define INTERVALSET_H
 
@@ -16,12 +16,11 @@
 //------------------------------------------------------------------ Types 
 
 struct Interval
-{ //Permet de gérer la liste doublement chaînée 
+{ //Permet de gérer la liste doublement chaînée
   Interval* suivant;
   Interval* precedent;
   long lLeft;
   long lRight;
-   
 } // Fin de Interval
 
 
@@ -43,21 +42,28 @@ public:
 	
   	bool AddInterval ( long lLeft, long lRight );
 	// Mode d'emploi (Ajout d'un intervalle) :
+	// Vérifie si l'intervalle est disjoint de la collection,
+	// si oui l'ajoute et renvoie TRUE, sinon ne fait rien et revoie FALSE
 	//
 	// Contrat : 
 	// lLeft <= lRight
 	
 	bool AddIntervalSet ( const IntervalSet& unIntervalSet );
-	// Mode d'emploi (Ajout d'un IntervalSet)
-	//
-	// Contrat : Argument non vide
+	// Mode d'emploi (Ajout d'un IntervalSet) :
+	// Prends en argument un objet IntervalSet, teste si l'ensemble des
+	// intervalles est disjoint de la collection, si oui les ajoute et
+	// renvoie TRUE, sinon ne fait rien et renvoie FALSE
 	// 
+	// Contrat :
+	// Argument correctement initialisé et contenant au moins un intervalle
 	
 	bool Remove ( long lPos );
-	// Mode d'emploi (Suppression d'un intervalle)
+	// Mode d'emploi (Suppression d'un intervalle) :
+	// Supprime l'intervalle désigné par son index dans la collection.
+	// Renvoie TRUE en cas de succès, et FALSE en cas d'erreur
 	//
 	// Contrat :
-	//
+	// 0 <= lPos <= IntervalSet.Count() -1
 
 	long Count ( );
 	// Mode d'emploi :
@@ -66,19 +72,21 @@ public:
 	// Contrat : Néant
 	
 	bool GetInterval ( long lPos, long& lLeft, long& lRight );
-	//Mode d'emploi :
+	// Mode d'emploi (Récupération d'un intervalle donné):
+	// Retourne les bornes de l'intervalle donné par son index dans la
+	// collection.
 	//
-	//Contrat :
-	//
+	// Contrat :
+	// 0 <= lPos <= IntervalSet.Count() -1
 	
 	IntervalSet& Union ( IntervalSet& unIntervalSet );
-	// Mode d'emploi
-	//
-	// Contrat
-	//
+	// Mode d'emploi (Réunion entre deux ensembles d'intervalles):
+	// Retourne l'union entre deux ensembles d'intervalles. 
+	// Contrat :
+	// 
 	
 	IntervalSet& Intersection ( IntervalSet& unIntervalSet );
-	// Mode d'emploi
+	// Mode d'emploi (Intersection entre deux ensembles d'intervalles) :
 	//
 	// Contrat
 	//
@@ -111,8 +119,9 @@ protected:
 	//Méthodes de tri et de gestion de la liste
 
 //----------------------------------------------------- Attributs protégés
-
-	//Liste chainée de tableaux de pointeurs vers des tableaux [lLeft, lRight]
+	int nombre_elements;
+	Interval* tete;
+	Interval* queue;
 
 };
 
